18ae055f972b967e3531f8c0df6b28561b0ead6c
backmeupplz <backmeupplz@gmail.com>
2026-05-27 12:41:21 -0700

    Harden randomness fulfillment with precommit reveal

--- packages/contracts/test/RandomnessEngine.t.sol
+++ packages/contracts/test/RandomnessEngine.t.sol
@@ -40,12 +40,14 @@ contract RandomnessEngineTest is Test {
     }
 
     function testAuthorizedConsumerRequestsAndConsumesFulfilledRandomness() public {
+        _commitNextWord(123456789);
         consumer.start(purpose);
         uint256 requestId = consumer.requestId();
 
         RandomnessEngine.Request memory stored = engine.request(requestId);
         assertEq(stored.requester, address(consumer));
         assertEq(stored.purposeHash, purpose);
+        assertEq(stored.randomnessCommitment, engine.randomnessCommitment(123456789));
         assertEq(stored.createdAt, block.timestamp);
         assertEq(stored.fulfilledAt, 0);
         assertEq(stored.randomWord, 0);
@@ -66,6 +68,7 @@ contract RandomnessEngineTest is Test {
     }
 
     function testUnauthorizedFulfillerCannotFulfill() public {
+        _commitNextWord(77);
         consumer.start(purpose);
         uint256 requestId = consumer.requestId();
         vm.expectRevert(
@@ -75,6 +78,7 @@ contract RandomnessEngineTest is Test {
     }
 
     function testConsumerIsBlockedWhileRequestPending() public {
+        _commitNextWord(77);
         consumer.start(purpose);
 
         vm.expectRevert(
@@ -86,6 +90,7 @@ contract RandomnessEngineTest is Test {
     }
 
     function testDoubleFulfillmentUnknownRequestAndCrossPurposeReplayRevert() public {
+        _commitNextWord(1);
         consumer.start(purpose);
         uint256 requestId = consumer.requestId();
 
@@ -126,6 +131,10 @@ contract RandomnessEngineTest is Test {
 
         vm.prank(owner);
         engine.unpause();
+        bytes32 commitment = engine.randomnessCommitment(99);
+        vm.prank(nextFulfiller);
+        engine.commitRandomness(commitment);
+        vm.roll(block.number + 1);
         consumer.start(purpose);
 
         vm.startPrank(nextFulfiller);
@@ -133,5 +142,99 @@ contract RandomnessEngineTest is Test {
         vm.stopPrank();
         consumer.resolve(purpose);
         assertEq(consumer.resolvedWord(), 99);
+    }
+
+    function testStrictPrecommitPreventsPostRequestWordSelection() public {
+        bytes32 commitment = _commitNextWord(111);
+        consumer.start(purpose);
+
+        RandomnessEngine.Request memory stored = engine.request(consumer.requestId());
+        assertEq(stored.randomnessCommitment, commitment);
+
+        bytes32 wrongCommitment = engine.randomnessCommitment(222);
+        assertNotEq(wrongCommitment, commitment);
+        vm.startPrank(fulfiller);
+        uint256 requestId = consumer.requestId();
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                RandomnessEngine.RandomnessCommitmentMismatch.selector, commitment, wrongCommitment
+            )
+        );
+        engine.fulfillRandomness(requestId, 222);
+
+        engine.fulfillRandomness(requestId, 111);
+        vm.stopPrank();
+        consumer.resolve(purpose);
+        assertEq(consumer.resolvedWord(), 111);
+    }
+
+    function testRequestRequiresPriorActiveCommitmentAndConsumesIt() public {
+        vm.expectRevert(RandomnessEngine.NoRandomnessCommitment.selector);
+        consumer.start(purpose);
+
+        bytes32 commitment = engine.randomnessCommitment(123);
+        vm.prank(fulfiller);
+        engine.commitRandomness(commitment);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                RandomnessEngine.RandomnessCommitmentNotActive.selector,
+                commitment,
+                uint64(block.number)
+            )
+        );
+        consumer.start(purpose);
+
+        vm.roll(block.number + 1);
+        consumer.start(purpose);
+
+        bytes32 nextPurpose = keccak256("battle:planet:7:mission:4");
+        vm.expectRevert(RandomnessEngine.NoRandomnessCommitment.selector);
+        consumer.start(nextPurpose);
+    }
+
+    function testOnlyFulfillerCanCommitAndPendingCommitmentCannotBeReplaced() public {
+        bytes32 firstCommitment = engine.randomnessCommitment(1);
+        bytes32 secondCommitment = engine.randomnessCommitment(2);
+
+        vm.prank(attacker);
+        vm.expectRevert(
+            abi.encodeWithSelector(RandomnessEngine.UnauthorizedFulfiller.selector, attacker)
+        );
+        engine.commitRandomness(firstCommitment);
+
+        vm.prank(fulfiller);
+        engine.commitRandomness(firstCommitment);
+
+        vm.prank(fulfiller);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                RandomnessEngine.RandomnessCommitmentAlreadyPending.selector, firstCommitment
+            )
+        );
+        engine.commitRandomness(secondCommitment);
+    }
+
+    function testOwnerCanExplicitlyAcceptCentralizedFulfillmentMode() public {
+        vm.prank(owner);
+        engine.setPrecommitRequired(false);
+
+        consumer.start(purpose);
+        uint256 requestId = consumer.requestId();
+        RandomnessEngine.Request memory stored = engine.request(requestId);
+        assertEq(stored.randomnessCommitment, bytes32(0));
+
+        vm.prank(fulfiller);
+        engine.fulfillRandomness(requestId, 987);
+
+        consumer.resolve(purpose);
+        assertEq(consumer.resolvedWord(), 987);
+    }
+
+    function _commitNextWord(uint256 randomWord) internal returns (bytes32 commitment) {
+        commitment = engine.randomnessCommitment(randomWord);
+        vm.prank(fulfiller);
+        engine.commitRandomness(commitment);
+        vm.roll(block.number + 1);
     }
 }
