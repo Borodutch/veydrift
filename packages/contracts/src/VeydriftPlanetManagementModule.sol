c2383190aac686f4ef2153d1c40d1ecfd2b55924
backmeupplz <backmeupplz@gmail.com>
2026-06-03 18:11:12 -0700

    Auto-collect resources before spending

--- packages/contracts/src/VeydriftPlanetManagementModule.sol
+++ packages/contracts/src/VeydriftPlanetManagementModule.sol
@@ -387,6 +387,7 @@ contract VeydriftPlanetManagementModule is VeydriftResourceReserves {
     }
 
     function _spend(uint256 planetId, Resources memory cost) private {
+        _settleResources(planetId);
         Resources storage available = _planets[planetId].resources;
         if (
             available.metal < cost.metal || available.crystal < cost.crystal
