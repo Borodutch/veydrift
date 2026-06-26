28ed68936d45ad873ed0a9eafc4e2ae6ab20e14b
Nikita Kolmogorov <backmeupplz@gmail.com>
2026-06-26 14:51:23 -0700

    Fix crawler defenders in attack battles

--- packages/contracts/src/VeydriftCombatModule.sol
+++ packages/contracts/src/VeydriftCombatModule.sol
@@ -547,7 +547,7 @@ contract VeydriftCombatModule is VeydriftResourceReserves {
         returns (uint256 units)
     {
         for (uint8 i = 0; i <= MAX_SHIP_ID;) {
-            if (i != uint8(Ship.SolarSatellite)) {
+            if (_isPlanetCombatShip(Ship(i))) {
                 units += _shipCounts[planetId][Ship(i)];
             }
             unchecked {
@@ -578,6 +578,10 @@ contract VeydriftCombatModule is VeydriftResourceReserves {
                 ++i;
             }
         }
+    }
+
+    function _isPlanetCombatShip(Ship ship) private pure returns (bool) {
+        return uint8(ship) <= uint8(Ship.Pathfinder) && ship != Ship.SolarSatellite;
     }
 
     function _battleRoundSnapshot(uint256 attackMissionId, FleetMission storage mission)
