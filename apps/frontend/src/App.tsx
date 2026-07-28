84069a49cee1d5d7326e14f4b4ab40c623b9afdd
backmeupplz <backmeupplz@gmail.com>
2026-07-28 12:53:44 -0700

    feat: add VEYDRIFT CCA bid surface

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import { DocsApp } from "./components/DocsPage";
+import { CcaApp } from "./CcaApp";
 import { FirstPlanetSettlementApp } from "./FirstPlanetSettlementApp";
 
 export function App() {
+  if (typeof window !== "undefined" && window.location.pathname.startsWith("/cca")) {
+    return <CcaApp />;
+  }
+
   if (typeof window !== "undefined" && window.location.pathname.startsWith("/docs")) {
     return <DocsApp />;
   }
